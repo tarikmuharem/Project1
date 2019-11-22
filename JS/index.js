47dccfcb3e0ada190da2cb01c785eb5a2c62d492
Tarik Muharem <tare98muharem@gmail.com>
2019-11-22 23:53:50 +0100

    Version 1.0

---
+++ JS/index.js
@@ -0,0 +1,30 @@
+var balls = document.getElementsByClassName("ball");
+document.onmousemove = function(){
+	var x = event.clientX * 100 / window.innerWidth + "%";
+	var y = event.clientY * 100 / window.innerHeight + "%";
+	for(var i = 0; i < 2; i++){
+		balls[i].style.left = x;
+		balls[i].style.top = y;
+		balls[i].style.transform = "translate(- "+x+",-"+y+")";
+	}
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000);
+}
